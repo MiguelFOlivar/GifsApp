ab31724d722e789ebbf598e73b60c2943bd0f760
Miguel Figueroa <91326484+MiguelFOlivar@users.noreply.github.com>
2024-03-01 18:39:30 -0600

    Se optimizan importaciones con archivo de barril

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
+import { AddCategory, GifGrid} from './components';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export const GifApp =  () =>{
